52c26742b151b152acf7060d912ebef3625bad92
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-07-26 19:31:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-const buttonEl=document.getElementById("roll-button");
-const diceEl=document.getElementById("dice");
-let historyList=[];
-const rollHistoryEl=document.getElementById("roll-history");
-function rollDice()
-{
-  const rollResult=Math.floor(Math.random()*6)+1;
-  const diceFace=getDiceFace(rollResult);
-  
-  diceEl.innerHTML=diceFace;
+const buttonEl = document.getElementById("roll-button");
+const diceEl = document.getElementById("dice");
+let historyList = [];
+const rollHistoryEl = document.getElementById("roll-history");
+
+function rollDice() {
+  const rollResult = Math.floor(Math.random() * 6) + 1;
+  const diceFace = getDiceFace(rollResult);
+
+  diceEl.innerHTML = diceFace;
   historyList.push(rollResult);
   updateRollHistory();
 }
-function updateRollHistory()
-{
-  rollHistoryEl.innerHTML="";
-  for(let i=0;i<historyList.length;i++)
-    {
-     const listItem=document.createElement("li");
-      listItem.innerHTML=`Roll ${i+1}: <span>${getDiceFace(historyList[i])}</span>`;
-      rollHistoryEl.appendChild(listItem);
-    }}
-function getDiceFace(rollResult){
-  switch(rollResult){
+
+function updateRollHistory() {
+  rollHistoryEl.innerHTML = "";
+  for (let i = 0; i < historyList.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(historyList[i])}</span>`;
+    rollHistoryEl.appendChild(listItem);
+  }
+}
+
+function getDiceFace(rollResult) {
+  switch (rollResult) {
     case 1:
-      return "&#9856";
+      return "&#9856;";
     case 2:
-      return "&#9857";
+      return "&#9857;";
     case 3:
-      return "&#9858";
+      return "&#9858;";
     case 4:
-      return "&#9859";
+      return "&#9859;";
     case 5:
-      return "&#9860";
+      return "&#9860;";
     case 6:
-      return "&#9861";
+      return "&#9861;";
     default:
       return "";
   }
 }
-buttonEl.addEventListener("click",()=>{
-diceEl.classList.add("roll-animation");
-  setTimeout(()->{
+
+buttonEl.addEventListener("click", () => {
+  diceEl.classList.add("roll-animation");
+  setTimeout(() => {
     diceEl.classList.remove("roll-animation");
-  rollDice();
-},1000);
-})
+    rollDice();
+  }, 1000);
+});
